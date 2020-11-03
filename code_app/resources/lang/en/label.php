ba2a138e212ae1943c81df63076f8d1cfc2e9923
Taturon <taturon5511@gmail.com>
2020-11-03 22:49:39 +0900

    [Add] ログインページの多言語化辞書を追加

---
+++ code_app/resources/lang/en/label.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	// Login
+	'mail' => 'E-mail Address',
+	'password' => 'Password',
+	'login' => 'Login',
+	'remember' => 'Remember me',
+	'forgot' => 'Forgot password ?',
+
+];
