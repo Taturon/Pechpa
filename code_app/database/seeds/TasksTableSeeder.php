6bde62767afd9bc19bb677123c133c392114fbdc
Taturon <taturon5511@gmail.com>
2020-11-07 15:42:02 +0900

    [Modify] 問題文の改行コードが認識されるように引用符を二重引用符に変更

--- code_app/database/seeds/TasksTableSeeder.php
+++ code_app/database/seeds/TasksTableSeeder.php
@@ -8,7 +8,7 @@ class TasksTableSeeder extends Seeder {
 	public function run() {
 		DB::table('tasks')->insert([[
 			'title' => 'FizzBuzz',
-			'statement' => '入力Nが与えられるので、1からNの範囲でNが\n3の倍数の時は「Fizz」\n5の倍数の時は「Buzz」\n15の倍数の時は「FizzBuzz」\nこれら以外の場合は数字\nをそれぞれ改行区切りで出力して下さい。',
+			'statement' => "入力Nが与えられるので、1からNの範囲でNが\n3の倍数の時は「Fizz」\n5の倍数の時は「Buzz」\n15の倍数の時は「FizzBuzz」\nこれら以外の場合は数字\nをそれぞれ改行区切りで出力して下さい。",
 			'input' => '入力は以下の形式で標準入力から与えられる。',
 			'input_code' => 'N',
 			'output' => '「Fizz」、「Buzz」、「FizzBuzz」、「数値」のいずれかを改行区切りで出力してください。',
