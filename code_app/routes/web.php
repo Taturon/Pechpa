e9276a1b22b9e1f4cf1b335290ddecfefb0d725e
Taturon <taturon5511@gmail.com>
2020-11-17 21:50:45 +0900

    [Modify] 回答のPOSTを認証状態でのみ実行できる様にルーティングを変更

--- code_app/routes/web.php
+++ code_app/routes/web.php
@@ -17,5 +17,11 @@
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'TaskController@index')->name('tasks.index');
-Route::post('tasks/{task}/answer', 'AnswerController@check')->name('answers.check');
 Route::resource('tasks', 'TaskController', ['only' => ['show']]);
+
+/**
+ * ユーザー認証状態でアクセス可
+ */
+Route::group(['middleware' => ['auth']], function() {
+	Route::post('tasks/{task}/answer', 'AnswerController@check')->name('answers.check');
+});
