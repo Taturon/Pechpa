aa15094c024d3138a553d2ae460d633f364eec8b
Taturon <taturon5511@gmail.com>
2020-11-18 21:17:25 +0900

    [Add] Answerコントローラーのcheckメソッドに提出されたコードを各テストケースで検証する処理を追加

--- code_app/app/Http/Controllers/AnswerController.php
+++ code_app/app/Http/Controllers/AnswerController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class AnswerController extends Controller {
 
 	public function check(Request $request, $id) {
-		$path = config('app.path') . '/storage/app/public/answer' . $id . '/';
+		$path = config('app.path') . '/storage/app/public/answer/' . $id . '/';
 		$path .= $request->user()->id . '/';
 		if (!File::exists($path)) {
 			File::makeDirectory($path, 0755, true);
 		}
 		$path .= floor(microtime(true)) . '.php';
 		File::put($path, $request->source);
+		$tests = Task::find($id)->tests;
+		$mismatches = 0;
+		foreach ($tests as $test) {
+			$process = new Process(['php', $path]);
+			$process->setInput($test->input);
+			$process->run();
+			$user_output = $process->getOutput();
+			if ($user_output !== $test->output) {
+				$mismatches++;
+			}
+		}
 	}
 }
