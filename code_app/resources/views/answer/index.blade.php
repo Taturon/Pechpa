5ed3dae2012c376fffb8ac89522838efbc26d0e1
Taturon <taturon5511@gmail.com>
2020-11-20 23:00:34 +0900

    [Add] 回答一覧ページを追加

---
+++ code_app/resources/views/answer/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+@section('title', __('title.answers_list'))
+@section('content')
+<div class="container">
+	<div class="page-header" style="margin-top:-30px;padding-bottom:0px;">
+		<h1><small>@lang('title.answers_list')</small></h1>
+	</div>
+	<div class="table-responsive">
+		<table class="table table-striped table-hover table-bordered">
+			<thead>
+				<tr>
+					<th style="width:10%;">
+						<div class="text-center">@lang('th.submission_datetime')</div>
+					</th>
+					<th style="width:70%;">
+						<div class="text-center">@lang('th.task_title')</div>
+					</th>
+					<th style="width:10%;">
+						<div class="text-center">@lang('th.task_rank')</div>
+					</th>
+					<th style="width:10%;">
+						<div class="text-center">@lang('th.judge')</div>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($answers as $answer)
+					<tr>
+						<td>
+							{{ $answer->created_at }}
+						</td>
+						<td>
+							<a href="{{ route('tasks.show', ['task' => $answer->task->id]) }}">{{ $answer->task->title }}</a>
+						</td>
+						<td style="color:{{ config('tasks.colors')[$answer->task->difficulty] }};">
+							{{ config('tasks.stars')[$answer->task->difficulty] }}
+						</td>
+						<td>
+							<div class="text-center">
+								<span class="label label-{{ $answer->judge === 'AC' ? 'success' : 'warning' }}">{{ $answer->judge }}</span>
+							</div>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+	<hr>
+</div>
+@endsection
