a5725c72336501971ffcc30fde60032757358118
Taturon <taturon5511@gmail.com>
2020-11-21 20:50:42 +0900

    [Add] TestモデルとTestingモデルに1対多のリレーションを追加

--- code_app/app/Models/Testing.php
+++ code_app/app/Models/Testing.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Testing extends Model {
 
 	protected $guarded = ['id'];
+
+	public function test() {
+		return $this->belongsTo('App\Models\Test');
+	}
 }
