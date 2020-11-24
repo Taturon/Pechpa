450262a1b660bf0d7481639462b5c61716c6a4b9
Taturon <taturon5511@gmail.com>
2020-11-24 19:54:17 +0900

    [Modify] TasksテーブルのマイグレーションファイルとシーダーにConstraintsカラムを追加

--- code_app/database/seeds/TasksTableSeeder.php
+++ code_app/database/seeds/TasksTableSeeder.php
@@ -9,6 +9,7 @@ class TasksTableSeeder extends Seeder {
 		DB::table('tasks')->insert([[
 			'title' => 'FizzBuzz',
 			'statement' => "入力Nが与えられるので、1からNの範囲でNが\n3の倍数の時は「Fizz」\n5の倍数の時は「Buzz」\n15の倍数の時は「FizzBuzz」\nこれら以外の場合は数字\nをそれぞれ改行区切りで出力して下さい。",
+			'constraints' => 'Nは整数かつ1 &#8806; N &#8806; 100',
 			'input' => '入力は以下の形式で標準入力から与えられる。',
 			'input_code' => 'N',
 			'output' => '「Fizz」、「Buzz」、「FizzBuzz」、「数値」のいずれかを改行区切りで出力してください。',
@@ -19,6 +20,7 @@ class TasksTableSeeder extends Seeder {
 		], [
 			'title' => 'フィボナッチ数列',
 			'statement' => 'フィボナッチ数列を入力Nまで出力して下さい。',
+			'constraints' => 'Nは整数かつ1 &#8806; N &#8806; 100',
 			'input' => '入力は以下の形式で標準入力から与えられる。',
 			'input_code' => 'N',
 			'output' => '指定された個数のフィボナッチ数列を改行区切りで出力してください。',
