ed0320b870b897032ba037093775a442395e5d3e
Taturon <taturon5511@gmail.com>
2020-11-30 22:37:49 +0900

    [Add] 管理者ログイン／ログアウト時のルーティングの雛形を追加

--- code_app/routes/web.php
+++ code_app/routes/web.php
@@ -26,3 +26,19 @@ Route::group(['middleware' => ['auth']], function() {
 	Route::resource('answers', 'AnswerController', ['only' => ['index', 'show']]);
 	Route::post('tasks/{task}/answer', 'AnswerController@check')->name('answers.check');
 });
+
+/**
+ * 管理者非認証状態でアクセス可
+ */
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\LoginController@login');
+});
+
+/**
+ * 管理者認証状態でアクセス可
+ */
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home', 'HomeController@index')->name('admin.home');
+});
