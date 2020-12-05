7e8f725c2fd2813732f27fe3ebd9b632a7192b4a
Taturon <taturon5511@gmail.com>
2020-12-05 20:51:39 +0900

    [Add] 問題作成通知メールの多言語化辞書を追加

---
+++ code_app/resources/lang/en/mail.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+	'subject_task_created' => 'Task Created Notification',
+	'header_task_created' => 'The following task has been created:',
+	'task_creater' => 'Creater',
+	'task_title' => 'Title',
+
+];
---
+++ code_app/resources/lang/ja/mail.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+	'subject_task_created' => '問題作成通知',
+	'header_task_created' => '問題が作成されました',
+	'task_creater' => '作成者',
+	'task_title' => 'タイトル',
+
+];
