c604b523c0fb6bd8c2941cb138e21dcd8351e151
Taturon <taturon5511@gmail.com>
2020-12-06 23:41:59 +0900

    [Modify] AnswerコントローラーからDB保存処理とコードチェック処理をリポジトリとサービスに分離 #37

--- code_app/app/Http/Controllers/AnswerController.php
+++ code_app/app/Http/Controllers/AnswerController.php
@@ -2,23 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
-use App\Models\Testing;
-use App\Models\Answer;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use App\Services\AnswerService;
+use App\Repositories\Task\TaskRepositoryInterface AS Task;
+use App\Repositories\Answer\AnswerRepositoryInterface AS Answer;
 
 class AnswerController extends Controller {
 
+	protected $answer;
+
+	protected $task;
+
+	protected $answer_service;
+
+	public function __construct(Answer $answer, Task $task, AnswerService $answer_service) {
+		$this->answer = $answer;
+		$this->task = $task;
+		$this->answer_service = $answer_service;
+	}
+
 	public function index() {
-		$answers = Answer::all();
+		$answers = $this->answer->all();
 		return view('answer.index', compact('answers'));
 	}
 
 	public function show($id) {
-		$answer = Answer::find($id);
+		$answer = $this->answer->findById($id);
 		if ($answer) {
 			$testings = $answer->testing;
 			return view('answer.show', compact('answer', 'testings'));
@@ -27,67 +36,22 @@ class AnswerController extends Controller {
 		}
 	}
 
-	public function check(Request $request, $id) {
-
-		// 回答ファイルの作成
-		$path = config('app.path') . '/storage/app/public/answer/';
-		if (!File::exists($path)) {
-			File::makeDirectory($path, 0755, true);
-		}
-		$path .= floor(microtime(true)) . '.php';
-		File::put($path, $request->source);
-
-		// 構文チェック
-		$process = new Process(['php', '-l', $path]);
-		$process->run();
-		$output = $process->getOutput();
-		$byte = filesize($path);
-		$compile_message = str_replace($path, 'your_answer.php', $output);
-
-		// DB保存処理
-		$answer = new Answer;
-		$answer->task_id = $id;
-		$answer->user_id = $request->user()->id;
-		$answer->answer_code = $request->source;
-		$answer->compile_message = $compile_message;
-		$answer->byte = $byte;
-		$answer->save();
-
-		// 各テストケースの判定
-		$tests = Task::find($id)->tests;
-		$mismatches = 0;
-		foreach ($tests as $test) {
-			$process = new Process(['php', $path]);
-			$process->setInput($test->input);
-			$process->run();
-			if ($process->getOutput() !== $test->output) {
-				$mismatches++;
-				$test_judge = 'WA';
-			} else {
-				$test_judge = 'AC';
-			}
-			$testing = new Testing;
-			$testing->test_id = $test->id;
-			$testing->user_id = $request->user()->id;
-			$testing->answer_id = $answer->id;
-			$testing->output = str_replace($path, 'your_answer.php', $process->getOutput());
-			$testing->judge = $test_judge;
-			$testing->save();
-		}
+	public function check(Request $request, $task_id) {
+		$path = $this->answer_service->makeAnswerFile($request);
+		$syntax_check_result = $this->answer_service->syntaxCheck($path);
+		$user_id = $request->user()->id;
+		$answer = $this->answer->storeSyntaxCheckResult($request->source, $syntax_check_result, $user_id, $task_id);
+		$tests = $this->task->findById($task_id)->tests;
+		$test_results = $this->answer_service->tryTestCases($path, $tests, $answer->id, $user_id);
+		$this->answer->storeTestingResults($test_results);
+		$mismatches = $this->answer_service->countMismatches($test_results);
 		if ($mismatches === 0) {
 			$judge = 'AC';
 		} else {
 			$judge = 'WA';
 		}
-
-		// 回答ファイルの削除
-		File::delete($path);
-
-		// DB保存処理
-		$answer->judge = $judge;
-		$answer->mismatches = $mismatches;
-		$answer->save();
-
+		$this->answer_service->deleteAnswerFile($path);
+		$this->answer->updateAnswerResults($answer->id, $judge, $mismatches);
 		if ($judge === 'AC') {
 			return redirect()->route('answers.show', ['answer' => $answer->id])->with('success', __('flash.correct_answer'));
 		} else {
