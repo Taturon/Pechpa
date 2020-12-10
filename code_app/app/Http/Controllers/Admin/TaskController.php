3cfceef2f02ce268ba7d92c1251a750643cdd626
Taturon <taturon5511@gmail.com>
2020-12-11 06:46:21 +0900

    [Add] 管理者のTaskコントローラーにupdateメソッドを追加

--- code_app/app/Http/Controllers/Admin/TaskController.php
+++ code_app/app/Http/Controllers/Admin/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreTask;
 use App\Http\Controllers\Controller;
 use App\Repositories\Task\TaskRepositoryInterface AS Task;
 
@@ -22,5 +23,19 @@ class TaskController extends Controller {
 	public function edit($id) {
 		$task = $this->task->findUnreviewedTask($id);
 		return view('admin.task.edit', compact('task'));
+	}
+
+	public function update(StoreTask $request, $task_id) {
+		$this->task->destroySampleCases($task_id);
+		$this->task->storeSampleCases($task_id, $request);
+		$this->task->destroyTestCases($task_id);
+		$this->task->storeTestCases($task_id, $request);
+		if ($request->has('approval')) {
+			$this->task->updateTaskWithApproval($task_id, $request);
+			return redirect()->route('admin.tasks.index')->with('success', __('flash.task_approved'));
+		} elseif ($request->has('update')) {
+			$this->task->updateTaskWithoutApproval($task_id, $request);
+			return redirect()->route('admin.tasks.index')->with('success', __('flash.task_updated'));
+		}
 	}
 }
