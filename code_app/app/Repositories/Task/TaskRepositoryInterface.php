79ee6ec1814b39bd69f08a7e1359870050224c36
Taturon <taturon5511@gmail.com>
2020-12-11 06:44:21 +0900

    [Add] TaskインターフェイスにTaskを更新するメソッド及びサンプルケース／テストケースを削除するメソッドを追記

--- code_app/app/Repositories/Task/TaskRepositoryInterface.php
+++ code_app/app/Repositories/Task/TaskRepositoryInterface.php
@@ -16,7 +16,15 @@ interface TaskRepositoryInterface {
 
 	public function storeTask($request);
 
+	public function updateTaskWithApproval($task_id, $request);
+
+	public function updateTaskWithoutApproval($task_id, $request);
+
 	public function storeSampleCases($task_id, $request);
 
+	public function destroySampleCases($task_id);
+
 	public function storeTestCases($task_id, $request);
+
+	public function destroyTestCases($task_id);
 }
