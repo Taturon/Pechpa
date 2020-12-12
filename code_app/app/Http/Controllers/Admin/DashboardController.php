fd4576e5866c983fa0df6e486ec176f51d4f24e5
Taturon <taturon5511@gmail.com>
2020-12-12 09:27:32 +0900

    [Add] 管理者用Dashboradコントローラーを追加しdashboardメソッドを追加

---
+++ code_app/app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Task\TaskRepositoryInterface AS Task;
+
+class DashboardController extends Controller {
+
+	protected $task;
+
+	public function __construct(Task $task) {
+		$this->task = $task;
+	}
+
+	public function dashboard() {
+		$tasks['approved'] = $this->task->recentApprovedTasks(config('limits.admin_approved_tasks'));
+		$tasks['unapproved'] = $this->task->recentUnapprovedTasks(config('limits.admin_unapproved_tasks'));
+		return view('admin.dashboard', compact('tasks'));
+	}
+}
