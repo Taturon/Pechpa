ebcc508e3d26f17313e9bc60528683ce4e15faee
Taturon <taturon5511@gmail.com>
2020-12-12 11:24:08 +0900

    [Modify] 管理者用承認待ち問題一覧ページのタイトルと見出しを修正

--- code_app/resources/views/admin/task/index.blade.php
+++ code_app/resources/views/admin/task/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
-@section('title', __('title.tasks_list'))
+@section('title', __('title.tasks_unapproved_list'))
 @section('content')
 <div class="page-header" style="margin-top:-30px;padding-bottom:0px;">
-	<h1><small>@lang('title.tasks_unreviewed_list')</small></h1>
+	<h1><small>@lang('title.tasks_unapproved_list')</small></h1>
 </div>
 @component('components.alert')
 @endcomponent
