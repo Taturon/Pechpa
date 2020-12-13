5b60d112afc357da110cb80882ed8fc7d199f6df
Taturon <taturon5511@gmail.com>
2020-12-13 11:52:56 +0900

    [Add] UserモデルにTaskモデルとの一対一のリレーションを追加

--- code_app/app/Models/User.php
+++ code_app/app/Models/User.php
@@ -5,25 +5,29 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Authenticatable {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	use Notifiable;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password',
+	];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+
+	public function task() {
+		return $this->hasOne('App\Models\Task');
+	}
 }
