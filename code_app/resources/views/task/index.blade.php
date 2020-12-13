1654b36969e9d2f9e9c73efb2b655e547ee3e2b1
Taturon <taturon5511@gmail.com>
2020-12-13 11:59:38 +0900

    [Add] 問題検索フォームを問題一覧ページで読み込み「作成者」及び「正答率」のカラムを追加 #36

--- code_app/resources/views/task/index.blade.php
+++ code_app/resources/views/task/index.blade.php
@@ -6,6 +6,9 @@
 </div>
 @component('components.alert')
 @endcomponent
+@component('components.tasks_search_form')
+@endcomponent
+<hr>
 <div class="table-responsive">
 	<table class="table table-striped table-hover table-bordered">
 		<thead>
@@ -13,8 +16,14 @@
 				<th style="width:10%;">
 					<div class="text-center">@lang('th.task_difficulty')</div>
 				</th>
-				<th style="width:90%;">
+				<th style="width:70%;">
 					<div class="text-center">@lang('th.task_title')</div>
+				</th>
+				<th style="width:10%;">
+					<div class="text-center">@lang('th.task_creator')</div>
+				</th>
+				<th style="width:10%;">
+					<div class="text-center">@lang('th.task_validity')</div>
 				</th>
 			</tr>
 		</thead>
@@ -26,6 +35,12 @@
 					</td>
 					<td>
 						<a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+					</td>
+					<td class="text-center">
+						{{ $task->user->name }}
+					</td>
+					<td>
+						{{ $task->examinees == 0 ? __('label.task_no_examinees') : $task->solved / $task->examinees }}
 					</td>
 				</tr>
 			@endforeach
