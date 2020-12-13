f46c6c27b4dcb47f827355d9d7c962e0120fe740
Taturon <taturon5511@gmail.com>
2020-12-13 12:02:11 +0900

    [Add] 検索フォームの多言語化辞書を追加

---
+++ code_app/resources/lang/en/words.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+	'tasks' => [
+		'title' => 'Title',
+		'title_search_placeholder' => 'Search by task title...',
+		'difficulty' => 'Difficulty',
+		'include_no_examinees' => 'Include "No Examinees"',
+		'lower_validity' => 'Validity Lower Limit',
+		'upper_validity' => 'Validity Upper Limit',
+		'validity' => [
+			'lower' => [
+				'0' => '0% or more',
+				'25' => '25% or more',
+				'50' => '50% or more',
+				'75' => '75% or more',
+			],
+			'upper' => [
+				'25' => '25% or less',
+				'50' => '50% or less',
+				'75' => '75% or less',
+				'100' => '100% or less',
+			],
+		],
+	],
+
+	'buttons' => [
+		'search' => 'Search',
+	],
+
+];
---
+++ code_app/resources/lang/ja/words.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+	'tasks' => [
+		'title' => '問題名',
+		'title_search_placeholder' => '問題名で検索',
+		'difficulty' => '難易度',
+		'include_no_examinees' => '「回答者無し」を含める',
+		'lower_validity' => '正答率下限',
+		'upper_validity' => '正答率上限',
+		'validity' => [
+			'lower' => [
+				'0' => '0%以上',
+				'25' => '25%以上',
+				'50' => '50%以上',
+				'75' => '75%以上',
+			],
+			'upper' => [
+				'25' => '25%以下',
+				'50' => '50%以下',
+				'75' => '75%以下',
+				'100' => '100%以下',
+			],
+		],
+	],
+
+	'buttons' => [
+		'search' => '検索',
+	],
+
+];
