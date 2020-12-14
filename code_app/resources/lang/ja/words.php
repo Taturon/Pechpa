b704229a1f3b3ab159f45145537ed9225bf158f1
Taturon <taturon5511@gmail.com>
2020-12-14 21:53:42 +0900

    [Add][Delete] 多言語化辞書のbuttonファイルの内容をwordsに統合しbuttonファイルを削除 #78

--- code_app/resources/lang/ja/words.php
+++ code_app/resources/lang/ja/words.php
@@ -35,6 +35,16 @@ return [
 
 	'buttons' => [
 		'search' => '検索',
+		'guest_login' => '簡単ログイン',
+		'or' => 'または',
+		'submission' => '提出',
+		'submission_confirm' => '提出しますか?',
+		'send' => '送信',
+		'send_confirm' => '送信しますか?',
+		'approval' => '承認',
+		'approval_confirm' => 'この問題を承認しますか？',
+		'update_only' => '更新のみ',
+		'update_only_confirm' => '更新しますか？',
 	],
 
 ];
