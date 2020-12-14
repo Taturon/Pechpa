042ef7b468f710387d4da6ec7cd35d0b311c40da
Taturon <taturon5511@gmail.com>
2020-12-14 20:33:42 +0900

    [Modify] 入力値が保持される様にselectタグの生成にFormファサードを使用する様に変更 #81

--- code_app/resources/views/components/tasks_search_form.blade.php
+++ code_app/resources/views/components/tasks_search_form.blade.php
@@ -13,12 +13,7 @@
 			<div class="form-group{{ $errors->has('difficulty') ? ' has-error' : '' }}">
 				<div class="text-center">
 					{{ Form::label('difficulty', __('words.tasks.difficulty')) }}
-					<select id="difficulty" name="difficulty" class="form-control">
-						<option value="">@lang('words.tasks.no_select')</option>
-						@foreach (config('tasks.stars') as $k => $star)
-							<option value="{{ $k }}">{{ $star }}</option>
-						@endforeach
-					</select>
+					{{ Form::select('difficulty', array_merge([__('words.tasks.no_select')], config('tasks.stars')), 0, ['class' => 'form-control']) }}
 				</div>
 			</div>
 		</div>
@@ -26,12 +21,13 @@
 			<div class="form-group{{ $errors->has('lower_validity') ? ' has-error' : '' }}">
 				<div class="text-center">
 					{{ Form::label('lower_validity', __('words.tasks.lower_validity')) }}
-					<select id="lower_validity" name="lower_validity" class="form-control">
-						<option value="">@lang('words.tasks.no_select')</option>
-						@foreach (config('tasks.lower_validity') as $k => $validity)
-							<option value="{{ $k }}">@lang('words.tasks.validity.lower.' . $validity)</option>
-						@endforeach
-					</select>
+					{{ Form::select('lower_validity', [
+						0 => __('words.tasks.no_select'),
+						1 => __('words.tasks.validity.lower', ['n' => 0]),
+						2 => __('words.tasks.validity.lower', ['n' => 25]),
+						3 => __('words.tasks.validity.lower', ['n' => 50]),
+						4 => __('words.tasks.validity.lower', ['n' => 75]),
+					], 0, ['class' => 'form-control']) }}
 				</div>
 			</div>
 		</div>
@@ -39,12 +35,13 @@
 			<div class="form-group{{ $errors->has('upper_validity') ? ' has-error' : '' }}">
 				<div class="text-center">
 					{{ Form::label('upper_validity', __('words.tasks.upper_validity')) }}
-					<select id="upper_validity" name="upper_validity" class="form-control">
-						<option value="">@lang('words.tasks.no_select')</option>
-						@foreach (config('tasks.upper_validity') as $k => $validity)
-							<option value="{{ $k }}">@lang('words.tasks.validity.upper.' . $validity)</option>
-						@endforeach
-					</select>
+					{{ Form::select('upper_validity', [
+						0 => __('words.tasks.no_select'),
+						1 => __('words.tasks.validity.upper', ['n' => 25]),
+						2 => __('words.tasks.validity.upper', ['n' => 50]),
+						3 => __('words.tasks.validity.upper', ['n' => 75]),
+						4 => __('words.tasks.validity.upper', ['n' => 100]),
+					], 0, ['class' => 'form-control']) }}
 				</div>
 			</div>
 		</div>
@@ -52,9 +49,8 @@
 			<div class="form-group">
 				<div class="text-center">
 					{{ Form::label('include_no_examinees', __('words.tasks.include_no_examinees')) }}
-					<p>
-						{{ Form::checkbox('include_no_examinees', '', '', ['style' => 'margin-top:0.6em;transform:scale(2.0);']) }}
-					</p>
+					<br>
+					{{ Form::checkbox('include_no_examinees', old('include_no_examinees'), '', ['style' => 'margin-top:0.6em;transform:scale(2.0);']) }}
 				</div>
 			</div>
 		</div>
