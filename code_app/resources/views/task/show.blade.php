4eb1d9b78be9c30bfa093ffe55187f8739a4b32b
Taturon <taturon5511@gmail.com>
2020-12-14 21:46:12 +0900

    [Modify] 多言語化辞書をwordsファイルからのみ呼び出す様に修正 #78

--- code_app/resources/views/task/show.blade.php
+++ code_app/resources/views/task/show.blade.php
@@ -12,7 +12,7 @@
 				<br>
 				<small>
 					<b>
-						@lang('title.task_difficulty')
+						@lang('words.tasks.difficulty')
 						<span style="color:{{ config('tasks.colors')[$task->difficulty] }};">
 							{{ config('tasks.stars')[$task->difficulty] }}
 						</span>
@@ -20,37 +20,37 @@
 				</small>
 			</h1>
 		</div>
-		<h2><b>@lang('title.task_statement')</b></h2>
+		<h2><b>@lang('words.tasks.statement')</b></h2>
 		<p>{!! nl2br(e($task->statement)) !!}</p>
-		<h2><b>@lang('title.task_constraints')</b></h2>
+		<h2><b>@lang('words.tasks.constraints')</b></h2>
 		<p>{{ $task->constraints }}</p>
-		<h2><b>@lang('title.task_input')</b></h2>
+		<h2><b>@lang('words.tasks.input')</b></h2>
 		<p>{{ $task->input }}</p>
 		<pre><code>{{ $task->input_code }}</code></pre>
-		<h2><b>@lang('title.task_output')</b></h2>
+		<h2><b>@lang('words.tasks.output')</b></h2>
 		<p>{{ $task->output }}</p>
 		@isset($task->output_code)
 			<pre><code>{{ $task->output_code }}</code></pre>
 		@endisset
 		<hr>
 		@foreach ($task->samples as $key => $sample)
-			<h3><b>@lang('title.sample_input')&thinsp;{{ $key + 1 }}</b></h3>
+			<h3><b>@lang('words.tasks.sample_input')&thinsp;{{ $key + 1 }}</b></h3>
 			<pre><code>{{ $sample->input_code }}</code></pre>
-			<h3><b>@lang('title.sample_output')&thinsp;{{ $key + 1 }}</b></h3>
+			<h3><b>@lang('words.tasks.sample_output')&thinsp;{{ $key + 1 }}</b></h3>
 			<pre><code>{{ $sample->output_code }}</code></pre>
 			<hr>
 		@endforeach
-		<h2><b>@lang('title.task_answer')</b></h2>
+		<h2><b>@lang('words.answers.answer')</b></h2>
 		{{ Form::open(['route' => ['answers.check', $task->id]]) }}
 			{{ Form::textarea('source', "&lt;?php\n", ['rows' => 15, 'style' => 'width:100%;', 'id' => 'tab']) }}
 			<hr>
-			<button class="btn btn-block btn-primary" name="submission" type="submit" onclick="return confirm('@lang('button.submission_confirm')');">
-				@lang('button.submission')
+			<button class="btn btn-block btn-primary" name="submission" type="submit" onclick="return confirm('@lang('words.buttons.submission_confirm')');">
+				@lang('words.buttons.submission')
 			</button>
 		{{ Form::close() }}
 		<hr>
 		<ol class="breadcrumb">
-			<li><a href="{{ route('tasks.index') }}">@lang('title.tasks_list')</a></li>
+			<li><a href="{{ route('tasks.index') }}">@lang('words.titles.tasks_list')</a></li>
 			<li class="active">{{ $task->title }}</li>
 		</ol>
 	</div>
