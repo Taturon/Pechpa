01df2aedad7f177824c002e1feafbadb8834b99e
Taturon <taturon5511@gmail.com>
2020-12-15 19:29:22 +0900

    [Add] 問題詳細ページのパンくずリストに回答一覧を追加

--- code_app/resources/views/task/show.blade.php
+++ code_app/resources/views/task/show.blade.php
@@ -63,6 +63,7 @@
 		<hr>
 		<ol class="breadcrumb">
 			<li><a href="{{ route('tasks.index') }}">@lang('words.titles.tasks_list')</a></li>
+			<li><a href="{{ route('answers.index') }}">@lang('words.titles.answers_list')</a></li>
 			<li class="active">{{ $task->title }}</li>
 		</ol>
 	</div>
