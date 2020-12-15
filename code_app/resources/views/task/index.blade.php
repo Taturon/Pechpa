2e1e6723984572e641874a4cfd6d1ef9694af158
Taturon <taturon5511@gmail.com>
2020-12-15 19:10:12 +0900

    [Add] 問題一覧ページに検索結果がない場合の表示を追加 #83

--- code_app/resources/views/task/index.blade.php
+++ code_app/resources/views/task/index.blade.php
@@ -11,46 +11,52 @@
 @component('components.tasks_search_form')
 @endcomponent
 <hr>
-<div class="table-responsive">
-	<table class="table table-striped table-hover table-bordered">
-		<thead>
-			<tr>
-				<th style="width:10%;">
-					<div class="text-center">@lang('words.tasks.difficulty')</div>
-				</th>
-				<th style="width:70%;">
-					<div class="text-center">@lang('words.tasks.title')</div>
-				</th>
-				<th style="width:10%;">
-					<div class="text-center">@lang('words.tasks.creator')</div>
-				</th>
-				<th style="width:10%;">
-					<div class="text-center">@lang('words.tasks.validity.validity')</div>
-				</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach ($tasks as $task)
+@if (count($tasks) > 0)
+	<div class="table-responsive">
+		<table class="table table-striped table-hover table-bordered">
+			<thead>
 				<tr>
-					<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
-						{{ config('tasks.stars')[$task->difficulty] }}
-					</td>
-					<td>
-						<a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
-					</td>
-					<td class="text-center">
-						{{ $task->user->name }}
-					</td>
-					<td class="text-center">
-						{{ $task->examinees == 0 ? __('words.tasks.no_examinees') : sprintf('%03.1f', $task->solved / $task->examinees * 100) . '%' }}
-					</td>
+					<th style="width:10%;">
+						<div class="text-center">@lang('words.tasks.difficulty')</div>
+					</th>
+					<th style="width:70%;">
+						<div class="text-center">@lang('words.tasks.title')</div>
+					</th>
+					<th style="width:10%;">
+						<div class="text-center">@lang('words.tasks.creator')</div>
+					</th>
+					<th style="width:10%;">
+						<div class="text-center">@lang('words.tasks.validity.validity')</div>
+					</th>
 				</tr>
-			@endforeach
-		</tbody>
-	</table>
-	<div class="text-center">
-		{{ $tasks->links() }}
+			</thead>
+			<tbody>
+				@foreach ($tasks as $task)
+					<tr>
+						<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
+							{{ config('tasks.stars')[$task->difficulty] }}
+						</td>
+						<td>
+							<a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+						</td>
+						<td class="text-center">
+							{{ $task->user->name }}
+						</td>
+						<td class="text-center">
+							{{ $task->examinees == 0 ? __('words.tasks.no_examinees') : sprintf('%03.1f', $task->solved / $task->examinees * 100) . '%' }}
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+		<div class="text-center">
+			{{ $tasks->links() }}
+		</div>
 	</div>
-</div>
+@else
+	<div class="row text-center">
+		<h1 style="color:lightgray;">@lang('words.notices.no_tasks')</h1>
+	</div>
+@endif
 <hr>
 @endsection
