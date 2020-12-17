68c1d6e19bfc574a9da130e7fb8200fa599fc5c4
Taturon <taturon5511@gmail.com>
2020-12-17 18:45:54 +0900

    [Modify] 管理者の問題承認／承認待ち問題一覧／承認済み問題詳細ページの文言をwordsファイルから呼び出すように修正 #78

--- code_app/resources/views/admin/task/show.blade.php
+++ code_app/resources/views/admin/task/show.blade.php
@@ -12,7 +12,7 @@
 				<br>
 				<small>
 					<b>
-						@lang('title.task_difficulty')
+						@lang('words.tasks.difficulty')
 						<span style="color:{{ config('tasks.colors')[$task->difficulty] }};">
 							{{ config('tasks.stars')[$task->difficulty] }}
 						</span>
@@ -20,29 +20,29 @@
 				</small>
 			</h1>
 		</div>
-		<h2><b>@lang('title.task_statement')</b></h2>
+		<h2><b>@lang('words.tasks.statement')</b></h2>
 		<p>{!! nl2br(e($task->statement)) !!}</p>
-		<h2><b>@lang('title.task_constraints')</b></h2>
+		<h2><b>@lang('words.tasks.constraints')</b></h2>
 		<p>{{ $task->constraints }}</p>
-		<h2><b>@lang('title.task_input')</b></h2>
+		<h2><b>@lang('words.tasks.input')</b></h2>
 		<p>{{ $task->input }}</p>
 		<pre><code>{{ $task->input_code }}</code></pre>
-		<h2><b>@lang('title.task_output')</b></h2>
+		<h2><b>@lang('words.tasks.output')</b></h2>
 		<p>{{ $task->output }}</p>
 		@isset($task->output_code)
 			<pre><code>{{ $task->output_code }}</code></pre>
 		@endisset
 		<hr>
-		<h2 class="text-center">@lang('title.sample_cases')</h2>
+		<h2 class="text-center">@lang('words.tasks.samples')</h2>
 		<div class="table-responsive">
 			<table class="table table-striped table-hover table-bordered">
 				<thead>
 					<tr>
 						<th style="width:50%;">
-							<div class="text-center">@lang('th.sample_input')</div>
+							<div class="text-center">@lang('words.tasks.sample_input')</div>
 						</th>
 						<th style="width:50%;">
-							<div class="text-center">@lang('th.sample_output')</div>
+							<div class="text-center">@lang('words.tasks.sample_output')</div>
 						</th>
 					</tr>
 				</thead>
@@ -65,16 +65,16 @@
 			</table>
 		</div>
 		<hr>
-		<h2 class="text-center">@lang('title.test_cases')</h2>
+		<h2 class="text-center">@lang('words.tasks.tests')</h2>
 		<div class="table-responsive">
 			<table class="table table-striped table-hover table-bordered">
 				<thead>
 					<tr>
 						<th style="width:50%;">
-							<div class="text-center">@lang('th.test_input')</div>
+							<div class="text-center">@lang('words.tasks.test_input')</div>
 						</th>
 						<th style="width:50%;">
-							<div class="text-center">@lang('th.test_output')</div>
+							<div class="text-center">@lang('words.tasks.test_output')</div>
 						</th>
 					</tr>
 				</thead>
@@ -98,9 +98,9 @@
 		</div>
 		<hr>
 		<ol class="breadcrumb">
-			<li><a href="{{ route('admin.dashboard') }}">@lang('link.admin_dashboard')</a></li>
-			<li><a href="{{ route('admin.tasks.index') }}">@lang('link.unapproved_tasks_list')</a></li>
-			<li class="active">@lang('link.approved_task')&thinsp;:&thinsp;{{ $task->title }}</li>
+			<li><a href="{{ route('admin.dashboard') }}">@lang('words.titles.admin_dashboard')</a></li>
+			<li><a href="{{ route('admin.tasks.index') }}">@lang('words.titles.unapproved_tasks_list')</a></li>
+			<li class="active">@lang('words.titles.approved_task')&thinsp;:&thinsp;{{ $task->title }}</li>
 		</ol>
 	</div>
 </div>
