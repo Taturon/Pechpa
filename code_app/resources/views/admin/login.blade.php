17728143f88d16c37a2740fd0b0b824701f11639
Taturon <taturon5511@gmail.com>
2020-12-17 18:44:23 +0900

    [Modify] 管理者ダッシュボード及びログインページの文言をwordsファイルから呼び出すように修正 #78

--- code_app/resources/views/admin/login.blade.php
+++ code_app/resources/views/admin/login.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
-@section('title', __('title.login'))
+@section('title', __('words.titles.login'))
 @section('content')
 <div class="row">
 	<div class="col-md-8 col-md-offset-2">
 		<div class="panel panel-default" style="background-color:lightyellow;">
-			<div class="panel-heading" style="background-color:lightyellow;">@lang('title.login')</div>
+			<div class="panel-heading" style="background-color:lightyellow;">@lang('words.titles.login')</div>
 
 			<div class="panel-body">
 				<form class="form-horizontal" method="POST" action="{{ route('admin.login') }}">
 					{{ csrf_field() }}
 
 					<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-						<label for="email" class="col-md-4 control-label">@lang('label.mail')</label>
+						<label for="email" class="col-md-4 control-label">@lang('words.authentication.mail')</label>
 
 						<div class="col-md-6">
 							<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +26,7 @@
 					</div>
 
 					<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-						<label for="password" class="col-md-4 control-label">@lang('label.password')</label>
+						<label for="password" class="col-md-4 control-label">@lang('words.authentication.password')</label>
 
 						<div class="col-md-6">
 							<input id="password" type="password" class="form-control" name="password" required>
@@ -42,7 +42,7 @@
 					<div class="form-group">
 						<div class="col-md-8 col-md-offset-4">
 							<button type="submit" class="btn btn-primary">
-								@lang('label.login')
+								@lang('words.buttons.login')
 							</button>
 						</div>
 					</div>
