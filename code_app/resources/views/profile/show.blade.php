b5bba201103a9a0bd8f86da2941f1db65329bc06
Taturon <taturon5511@gmail.com>
2020-12-19 12:35:50 +0900

    [Modify] ユーザープロフィールページのビューを格納するフォルダ名を修正しファイル名をメソッド名と同じ名前に修正

--- code_app/resources/views/profile/show.blade.php
+++ code_app/resources/views/profile/show.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
-@section('title', __('words.titles.user_profile'))
+@section('title', __('words.titles.profile'))
 @section('content')
 <div class="row">
 	<div class="col-md-12">
 		<div class="page-header" style="margin-top:-20px;padding-bottom:0px;">
-			<h1>@lang('words.titles.user_profile')</h1>
+			<h1>@lang('words.titles.profile')</h1>
 		</div>
 		<div class="row">
-				<div class="text-center">
-					<h2>
-						<img src="{{ asset('storage/icons/' . Auth::user()->icon) }}" width="150px" height="150px">
-					</h2>
+			<div class="text-center">
+				<div class="col-md-12 text-center">
+					<img src="{{ asset('storage/icons/' . Auth::user()->icon) }}" width="150px" height="150px">
+				</div>
 				<div class="col-md-6 text-right">
 					<h2>@lang('words.users.registered_date')</h2>
 				</div>
 				<div class="col-md-6 text-left">
-					<h2>{{ Auth::user()->created_at }}</h2>
+					<h2>{{ $user->created_at }}</h2>
 				</div>
 				<div class="col-md-6 text-right">
 					<h2>@lang('words.users.name')</h2>
 				</div>
 				<div class="col-md-6 text-left">
-					<h2>{{ Auth::user()->name }}</h2>
+					<h2>{{ $user->name }}</h2>
 				</div>
 				<div class="col-md-6 text-right">
 					<h2>@lang('words.users.email')</h2>
 				</div>
 				<div class="col-md-6 text-left">
-					<h2>{{ Auth::user()->email }}</h2>
+					<h2>{{ $user->email }}</h2>
 				</div>
 				<div class="col-md-6 text-right">
 					<h2>@lang('words.users.approved_tasks_count')</h2>
@@ -45,7 +45,7 @@
 					<h2>@lang('words.users.correct_answer_rate')</h2>
 				</div>
 				<div class="col-md-6 text-left">
-					<h2>{{ $statics['correct_answer_rate'] }}</h2>
+					<h2>{{ $statics['correct_rate'] }}</h2>
 				</div>
 			</div>
 		</div>
@@ -53,7 +53,8 @@
 		<ol class="breadcrumb">
 			<li><a href="{{ route('tasks.index') }}">@lang('words.titles.tasks_list')</a></li>
 			<li><a href="{{ route('answers.index') }}">@lang('words.titles.answers_list')</a></li>
-			<li class="active">@lang('words.titles.user_profile')</li>
+			<li class="active">@lang('words.titles.profile')</li>
+			<li><a href="{{ route('profiles.edit', ['id' => Auth::user()->id]) }}">@lang('words.titles.profile_edit')</a></li>
 		</ol>
 	</div>
 </div>
