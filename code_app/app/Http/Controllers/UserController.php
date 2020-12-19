c22d049434d6d07cac2937487b6973fa1df3ffa7
Taturon <taturon5511@gmail.com>
2020-12-19 12:22:49 +0900

    [Modify][Add] UserコントローラーでUserリポジトリを用いるようにしedit及びupdateメソッドを追記

--- code_app/app/Http/Controllers/UserController.php
+++ code_app/app/Http/Controllers/UserController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\UserService;
 use Illuminate\Http\Request;
+use App\Services\UserService;
+use App\Repositories\User\UserRepositoryInterface AS User;
 
 class UserController extends Controller {
 
 	protected $service;
 
-	public function __construct(UserService $service) {
+	public function __construct(User $repository, UserService $service) {
+		$this->repository = $repository;
 		$this->service = $service;
 	}
 
-	public function showProfile() {
-		$statics = $this->service->getUserStatistics();
-		return view('user.profile', compact('statics'));
+	public function show($user_id) {
+		$user = $this->repository->findById($user_id);
+		$statics['approved_tasks'] = $this->repository->countApprovedTasks($user_id);
+		$statics['all_answers'] = $this->repository->countAllAnswers($user_id);
+		$statics['correct_answers'] = $this->repository->countCorrectAnswers($user_id);
+		$statics['correct_rate'] = $this->service->calculateCorrectRate($statics['all_answers'], $statics['correct_answers']);
+		return view('profile.show', compact(['user', 'statics']));
+	}
+
+	public function edit() {
+	}
+
+	public function update() {
 	}
 }
