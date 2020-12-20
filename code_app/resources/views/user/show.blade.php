f0563a82bdb0d00dfe276f14bdfcb0ba22840225
Taturon <taturon5511@gmail.com>
2020-12-20 21:37:35 +0900

    [Modify] プロフィールページの各style属性を追加したCSSファイルから呼び出すように修正し各項目をtableタグ内で表示させることでレスポンシブに対応

--- code_app/resources/views/user/show.blade.php
+++ code_app/resources/views/user/show.blade.php
@@ -2,9 +2,9 @@
 @section('title', __('words.titles.profile', ['name' => $user->name]))
 @section('content')
 <div class="row">
-	<div class="col-md-12">
-		<div class="page-header" style="margin-top:-20px;padding-bottom:0px;">
-			<h1>@lang('words.titles.profile', ['name' => $user->name])</h1>
+	<div id="content" class="col-md-12">
+		<div id="page-header" class="page-header">
+			<h1 class="text-center">@lang('words.titles.profile', ['name' => $user->name])</h1>
 		</div>
 		@component('components.alert')
 		@endcomponent
@@ -13,59 +13,77 @@
 				<div class="col-md-12 text-center">
 					<img src="{{ asset('storage/icons/' . $user->icon) }}" width="150px" height="150px">
 					@if ($user->id === Auth::user()->id)
-						<p style="font-size:15px">
+						<p id="edit-link">
 							<a href="{{ route('users.edit', ['user_id' => Auth::user()->id]) }}">
 								@lang('words.buttons.edit')
 							</a>
 						</p>
 					@endif
 				</div>
-				<div class="col-md-6 text-right">
-					<h2>@lang('words.users.registered_date')</h2>
-				</div>
-				<div class="col-md-6 text-left">
-					<h2>{{ $user->created_at }}</h2>
-				</div>
-				<div class="col-md-6 text-right">
-					<h2>@lang('words.users.name')</h2>
-				</div>
-				<div class="col-md-6 text-left">
-					<h2>
-						{{ $user->name }}
-						@if ($user->id === Auth::user()->id)
-							<span style="font-size:15px">
-								<a href="{{ route('users.edit', ['user_id' => Auth::user()->id]) }}">
-									@lang('words.buttons.edit')
-								</a>
-							</span>
-						@endif
-					</h2>
-				</div>
-				@if ($user->id === Auth::user()->id)
-					<div class="col-md-6 text-right">
-						<h2>@lang('words.users.email')</h2>
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<div class="table-responsive">
+						<table class="table table-hover ">
+							<tr>
+								<th class="text-right">
+									<h2>@lang('words.users.registered_date')</h2>
+								</th>
+								<td>
+									<h2>{{ $user->created_at }}</h2>
+								</td>
+							</tr>
+							<tr>
+								<th class="text-right">
+									<h2>@lang('words.users.name')</h2>
+								</th>
+								<td>
+									<h2>
+										{{ $user->name }}
+										@if ($user->id === Auth::user()->id)
+											<span id="edit-link">
+												<a href="{{ route('users.edit', ['user_id' => Auth::user()->id]) }}">
+													@lang('words.buttons.edit')
+												</a>
+											</span>
+										@endif
+									</h2>
+								</td>
+							</tr>
+							@if ($user->id === Auth::user()->id)
+								<tr>
+									<th class="text-right">
+										<h2>@lang('words.users.email')</h2>
+									</th>
+									<td>
+										<h2>{{ $user->email }}</h2>
+									</td>
+								</tr>
+							@endif
+							<tr>
+								<th class="text-right">
+									<h2>@lang('words.users.approved_tasks_count')</h2>
+								</th>
+								<td>
+									<h2>{{ $statics['approved_tasks'] }}</h2>
+								</td>
+							</tr>
+							<tr>
+								<th class="text-right">
+									<h2>@lang('words.users.correct_answers_count')&thinsp;/&thinsp;@lang('words.users.all_answers_count')</h2>
+								</th>
+								<td>
+									<h2>{{ $statics['correct_answers'] }}&thinsp;/&thinsp;{{ $statics['all_answers'] }}</h2>
+								</td>
+							</tr>
+							<tr>
+								<th class="text-right">
+									<h2>@lang('words.users.correct_answer_rate')</h2>
+								</th>
+								<td>
+									<h2>{{ $statics['correct_rate'] }}</h2>
+								</td>
+							</tr>
+						</table>
 					</div>
-					<div class="col-md-6 text-left">
-						<h2>{{ $user->email }}</h2>
-					</div>
-				@endif
-				<div class="col-md-6 text-right">
-					<h2>@lang('words.users.approved_tasks_count')</h2>
-				</div>
-				<div class="col-md-6 text-left">
-					<h2>{{ $statics['approved_tasks'] }}</h2>
-				</div>
-				<div class="col-md-6 text-right">
-					<h2>@lang('words.users.correct_answers_count')&thinsp;/&thinsp;@lang('words.users.all_answers_count')</h2>
-				</div>
-				<div class="col-md-6 text-left">
-					<h2>{{ $statics['correct_answers'] }}&thinsp;/&thinsp;{{ $statics['all_answers'] }}</h2>
-				</div>
-				<div class="col-md-6 text-right">
-					<h2>@lang('words.users.correct_answer_rate')</h2>
-				</div>
-				<div class="col-md-6 text-left">
-					<h2>{{ $statics['correct_rate'] }}</h2>
 				</div>
 			</div>
 		</div>
