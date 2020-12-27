5572fd7ebd04075e301bd825e8b03a5b8c1bde49
Taturon <taturon5511@gmail.com>
2020-12-27 09:33:02 +0900

    [Modify] ユーザーが作成した問題一覧ページの表示項目を修正

--- code_app/resources/views/user/task/index.blade.php
+++ code_app/resources/views/user/task/index.blade.php
@@ -9,8 +9,47 @@
 	@endcomponent
 	<h2 class="text-center">@lang('words.titles.approved_tasks')</h2>
 	@if (count($approved_tasks) > 0)
-		@component('components.tasks_index', ['tasks' => $approved_tasks])
-		@endcomponent
+		<div class="table-responsive">
+			<table class="table table-striped table-hover table-bordered">
+				<thead>
+					<tr>
+						<th style="width:15%;">
+							<div class="text-center">@lang('words.tasks.difficulty')</div>
+						</th>
+						<th style="width:55%;">
+							<div class="text-center">@lang('words.tasks.title')</div>
+						</th>
+						<th style="width:15%;">
+							<div class="text-center">@lang('words.tasks.validity.validity')</div>
+						</th>
+						<th style="width:15%;">
+							<div class="text-center">@lang('words.tasks.approved_date')</div>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($approved_tasks as $task)
+						<tr>
+							<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
+								{{ config('tasks.stars')[$task->difficulty] }}
+							</td>
+							<td>
+								<a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+							</td>
+							<td class="text-center">
+								{{ $task->examinees == 0 ? __('words.tasks.no_examinees') : sprintf('%03.1f', $task->solved / $task->examinees * 100) . '%' }}
+							</td>
+							<td class="text-center">
+								{{ $task->reviewed_at }}
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+			<div class="text-center">
+				{{ $approved_tasks->appends(request()->input())->links() }}
+			</div>
+		</div>
 	@else
 		<div class="row text-center">
 			<h1 style="color:lightgray;">@lang('words.notices.no_approved_tasks', ['name' => $user->name])</h1>
@@ -20,8 +59,43 @@
 	@if ($user->id === Auth::user()->id)
 		<h2 class="text-center">@lang('words.titles.unapproved_tasks')</h2>
 		@if (count($unapproved_tasks) > 0)
-			@component('components.tasks_index', ['tasks' => $unapproved_tasks])
-			@endcomponent
+			<div class="table-responsive">
+				<table class="table table-striped table-hover table-bordered">
+					<thead>
+						<tr>
+							<th style="width:20%;">
+								<div class="text-center">@lang('words.tasks.difficulty')</div>
+							</th>
+							<th style="width:60%;">
+								<div class="text-center">@lang('words.tasks.title')</div>
+							</th>
+							<th style="width:20%;">
+								<div class="text-center">@lang('words.tasks.created_and_updated_date')</div>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($unapproved_tasks as $task)
+							<tr>
+								<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
+									{{ config('tasks.stars')[$task->difficulty] }}
+								</td>
+								<td>
+									<a href="{{ route('tasks.edit', ['task' => $task->id]) }}">
+										{{ $task->title }}
+									</a>
+								</td>
+								<td class="text-center">
+									{{ $task->updated_at }}
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+				<div class="text-center">
+					{{ $unapproved_tasks->appends(request()->input())->links() }}
+				</div>
+			</div>
 		@else
 			<div class="row text-center">
 				<h1 style="color:lightgray;">@lang('words.notices.no_unapproved_tasks', ['name' => $user->name])</h1>
@@ -30,8 +104,14 @@
 		<hr>
 	@endif
 	<ol class="breadcrumb">
-		<li><a href="{{ route('users.show', ['user_id' => Auth::user()->id]) }}"><i class="fas fa-address-card"></i>&thinsp;@lang('words.titles.your_profile')</a></li>
-		<li class="active"><i class="fas fa-code"></i>&thinsp;@lang('words.titles.created_tasks', ['name' => $user->name])</li>
+		<li>
+			<a href="{{ route('users.show', ['user' => $user->id]) }}">
+				<i class="fas fa-address-card"></i>&thinsp;@lang('words.titles.your_profile')
+			</a>
+		</li>
+		<li class="active">
+			<i class="fas fa-code"></i>&thinsp;@lang('words.titles.created_tasks', ['name' => $user->name])
+		</li>
 	</ol>
 </div>
 @endsection
