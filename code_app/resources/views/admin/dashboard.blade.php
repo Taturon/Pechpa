17645c0894c00759938d944acf063900a6ecfedb
Taturon <taturon5511@gmail.com>
2020-12-28 09:51:56 +0900

    [Modify] 管理者ダッシュボードの直近の承認待ち／直近の承認済み問題一覧の部分をビューコンポーネントに置換

--- code_app/resources/views/admin/dashboard.blade.php
+++ code_app/resources/views/admin/dashboard.blade.php
@@ -8,38 +8,8 @@
 	@component('components.alert')
 	@endcomponent
 	@if (count($tasks['unapproved']) > 0)
-		<div class="table-responsive">
-			<table class="table table-striped table-hover table-bordered">
-				<thead>
-					<tr>
-						<th style="width:15%;">
-							<div class="text-center">@lang('words.tasks.created_date')</div>
-						</th>
-						<th style="width:75%;">
-							<div class="text-center">@lang('words.tasks.title')</div>
-						</th>
-						<th style="width:10%;">
-							<div class="text-center">@lang('words.tasks.difficulty')</div>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach ($tasks['unapproved'] as $task)
-						<tr>
-							<td>
-								{{ $task->created_at }}
-							</td>
-							<td>
-								<a href="{{ route('admin.tasks.edit', ['id' => $task->id]) }}">{{ $task->title }}</a>
-							</td>
-							<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
-								{{ config('tasks.stars')[$task->difficulty] }}
-							</td>
-						</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
+		@component('components.tasks_index_pages.unapproved', ['tasks' => $tasks['unapproved']])
+		@endcomponent
 	@else
 		<div class="row text-center">
 			<h1 style="color:lightgray;">@lang('words.notices.tasks_no_unapproved_list')</h1>
@@ -49,38 +19,8 @@
 		<h1>@lang('words.titles.tasks_recent_approved_list')</h1>
 	</div>
 	@if (count($tasks['approved']) > 0)
-	<div class="table-responsive">
-		<table class="table table-striped table-hover table-bordered">
-			<thead>
-				<tr>
-					<th style="width:15%;">
-						<div class="text-center">@lang('words.tasks.published_date')</div>
-					</th>
-					<th style="width:75%;">
-						<div class="text-center">@lang('words.tasks.title')</div>
-					</th>
-					<th style="width:10%;">
-						<div class="text-center">@lang('words.tasks.difficulty')</div>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($tasks['approved'] as $task)
-					<tr>
-						<td>
-							{{ $task->reviewed_at }}
-						</td>
-						<td>
-							<a href="{{ route('admin.tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-						</td>
-						<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
-							{{ config('tasks.stars')[$task->difficulty] }}
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
+		@component('components.tasks_index_pages.approved', ['tasks' => $tasks['approved']])
+		@endcomponent
 	@else
 		<div class="row text-center">
 			<h1 style="color:lightgray;">@lang('words.notices.tasks_no_approved_list')</h1>
