2bbfba477fe3b5a681e5e6cf3476cadeaaabe8f0
Taturon <taturon5511@gmail.com>
2020-12-28 19:26:00 +0900

    [Modify] Testingsテーブルの不要な外部キーtest_id／user_idをマイグレーションファイルから削除

--- code_app/database/migrations/2020_11_19_192344_create_testings_table.php
+++ code_app/database/migrations/2020_11_19_192344_create_testings_table.php
@@ -9,8 +9,6 @@ class CreateTestingsTable extends Migration {
 	public function up() {
 		Schema::create('testings', function (Blueprint $table) {
 			$table->increments('id');
-			$table->unsignedInteger('test_id');
-			$table->unsignedInteger('user_id');
 			$table->unsignedInteger('answer_id');
 			$table->string('output', 500);
 			$table->string('judge', 50);
