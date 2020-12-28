119ac351a718e497e7522538fb09665d2b971191
Taturon <taturon5511@gmail.com>
2020-12-28 19:41:34 +0900

    [Modify] Answersテーブルのマイグレーションファイルの外部キーの順序を修正

--- code_app/database/migrations/2020_11_16_214205_create_answers_table.php
+++ code_app/database/migrations/2020_11_16_214205_create_answers_table.php
@@ -9,8 +9,8 @@ class CreateAnswersTable extends Migration {
 	public function up() {
 		Schema::create('answers', function (Blueprint $table) {
 			$table->increments('id');
-			$table->unsignedInteger('task_id');
 			$table->unsignedInteger('user_id');
+			$table->unsignedInteger('task_id');
 			$table->string('answer_code', 500);
 			$table->string('compile_message', 500);
 			$table->unsignedInteger('byte');
