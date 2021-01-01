1f337dac200a800f02d979bc3005f77075c46dfa
Taturon <taturon5511@gmail.com>
2021-01-01 13:08:16 +0900

    [Add] 管理者Userコントローラーにshowメソッドを追加

--- code_app/app/Http/Controllers/Admin/UserController.php
+++ code_app/app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Services\UserService;
 use App\Http\Controllers\Controller;
 use App\Repositories\User\UserRepositoryInterface AS User;
 
@@ -10,12 +11,28 @@ class UserController extends Controller {
 
 	protected $user;
 
-	public function __construct(User $user) {
+	protected $user_service;
+
+	public function __construct(User $user, UserService $user_service) {
 		$this->user = $user;
+		$this->user_service = $user_service;
 	}
 
 	public function index() {
 		$users = $this->user->all()->paginate(config('configs.pageings.admin_users'));
 		return view('admin.user.index', compact('users'));
+	}
+
+	public function show($user_id) {
+		$user = $this->user->findById($user_id);
+		if (is_null($user)) {
+			return redirect()->route('admin.users.index')->with('error', __('words.flashes.no_user'));
+		}
+		$statics['unapproved_tasks'] = $this->user->countUnapprovedTasks($user_id);
+		$statics['approved_tasks'] = $this->user->countApprovedTasks($user_id);
+		$statics['all_answers'] = $this->user->countAllAnswers($user_id);
+		$statics['correct_answers'] = $this->user->countCorrectAnswers($user_id);
+		$statics['correct_rate'] = $this->user_service->calculateCorrectRate($statics['all_answers'], $statics['correct_answers']);
+		return view('user.show', compact(['user', 'statics']));
 	}
 }
