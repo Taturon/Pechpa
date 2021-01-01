dce5380e905462e4ec580fc3257c1c74f0838901
Taturon <taturon5511@gmail.com>
2021-01-01 13:39:33 +0900

    [Add] 管理者向けユーザー詳細ページにパンくずリストを追加

--- code_app/resources/views/admin/user/show.blade.php
+++ code_app/resources/views/admin/user/show.blade.php
@@ -10,5 +10,20 @@
 		@component('components.user_profile', ['user' => $user, 'statics' => $statics])
 		@endcomponent
 		<hr>
+		<ol class="breadcrumb">
+			<li>
+				<a href="{{ route('admin.dashboard') }}">
+					<i class="fas fa-list"></i>&thinsp;@lang('words.titles.admin_dashboard')
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('admin.users.index') }}">
+					<i class="fas fa-users"></i>&thinsp;@lang('words.titles.users_list')
+				</a>
+			</li>
+			<li class="active">
+				<i class="fas fa-user"></i>&thinsp;@lang('words.users.name')&thinsp;:&thinsp;{{ $user->name }}
+			</li>
+		</ol>
 	</div>
 @endsection
