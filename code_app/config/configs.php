3bfaef8988e91b138ff8ffed67447b0eabe16bf3
Taturon <taturon5511@gmail.com>
2021-01-01 11:27:53 +0900

    [Add] 管理者のユーザー一覧ページのページネートのアイテム数のconfigファイルを追加

--- code_app/config/configs.php
+++ code_app/config/configs.php
@@ -12,4 +12,14 @@ return [
 		],
 	],
 
+	'pagings' => [
+		'user_tasks' => 10,
+		'user_answers' => 10,
+		'user_created_tasks' => 5,
+		'admin_approved_tasks' => 15,
+		'admin_unapproved_tasks' => 15,
+		'admin_inquiries' => 15,
+		'admin_users' => 15,
+	],
+
 ];
