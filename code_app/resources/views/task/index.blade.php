193c1c6e9cf1e0a91571fb168cd47cef72825ce1
Taturon <taturon5511@gmail.com>
2021-01-09 11:00:36 +0900

    [Modify] 問題一覧ページをログイン／非ログインでビューコンポーネントから条件分岐で呼び出す様に修正

--- code_app/resources/views/task/index.blade.php
+++ code_app/resources/views/task/index.blade.php
@@ -19,50 +19,13 @@
 	@endcomponent
 	<hr>
 	@if (count($tasks) > 0)
-		<div class="table-responsive">
-			<table class="table table-striped table-hover table-bordered">
-				<thead>
-					<tr>
-						<th style="width:15%;">
-							<div class="text-center">@lang('words.tasks.difficulty')</div>
-						</th>
-						<th style="width:55%;">
-							<div class="text-center">@lang('words.tasks.title')</div>
-						</th>
-						<th style="width:15%;">
-							<div class="text-center">@lang('words.tasks.creator')</div>
-						</th>
-						<th style="width:15%;">
-							<div class="text-center">@lang('words.tasks.validity.validity')</div>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach ($tasks as $task)
-						<tr>
-							<td style="color:{{ config('tasks.colors')[$task->difficulty] }};">
-								{{ config('tasks.stars')[$task->difficulty] }}
-							</td>
-							<td>
-								<a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
-							</td>
-							<td class="text-center">
-								<a href="{{ route('users.show', ['user' => $task->user->id]) }}">
-									<img src="{{ asset('storage/icons/' . $task->user->icon) }}" width="20px" height="20px">
-									{{ $task->user->name }}
-								</a>
-							</td>
-							<td class="text-center">
-								{{ $task->examinees == 0 ? __('words.tasks.no_examinees') : sprintf('%03.1f', $task->solved / $task->examinees * 100) . '%' }}
-							</td>
-						</tr>
-					@endforeach
-				</tbody>
-			</table>
-		</div>
-		<div class="text-center">
-			{{ $tasks->appends(request()->input())->links() }}
-		</div>
+		@if (Auth::guard('user')->check())
+			@component('components.tasks_index_pages.after_login', ['tasks' => $tasks])
+			@endcomponent
+		@else
+			@component('components.tasks_index_pages.before_login', ['tasks' => $tasks])
+			@endcomponent
+		@endif
 	@else
 		<div class="row text-center">
 			<h1 id="notice-block">@lang('words.notices.no_tasks')</h1>
