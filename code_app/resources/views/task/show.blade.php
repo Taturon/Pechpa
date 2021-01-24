86e3d1f5ec6826667b3c2dcdc5ef15e2ec665808
Taturon <taturon5511@gmail.com>
2021-01-24 18:57:29 +0900

    [Modify] ユーザー側の問題詳細ページでビューコンポーネントを呼び出すように修正 #182

--- code_app/resources/views/task/show.blade.php
+++ code_app/resources/views/task/show.blade.php
@@ -5,64 +5,8 @@
 @section('title', $task->title)
 @section('content')
 	<div id="content" class="col-md-12">
-		<div id="page-header" class="page-header">
-			<h1>
-				<b>{{ $task->title }}</b>
-			</h1>
-			<div class="row">
-				<div id="page-header-h2" class="col-md-3">
-					<h2>
-						<b>
-							<small>@lang('words.tasks.difficulty')&#65306;
-							<span style="color:{{ config('tasks.colors')[$task->difficulty] }};">
-								{{ config('tasks.stars')[$task->difficulty] }}
-							</span>
-							</small>
-						</b>
-					</h2>
-				</div>
-				<div id="page-header-h2" class="col-md-3">
-					<h2>
-						<b>
-							<small>@lang('words.tasks.solved')&#65306;</small>
-							<span>{{ $task->solved }}</span>
-						</b>
-					</h2>
-				</div>
-				<div id="page-header-h2" class="col-md-3">
-					<h2>
-						<b>
-							<small>@lang('words.tasks.examinees')&#65306;</small>
-							<span>{{ $task->examinees }}</span>
-						</b>
-					</h2>
-				</div>
-				<div id="page-header-h2" class="col-md-3">
-					<h2>
-						<b>
-							<small>@lang('words.tasks.validity.validity')&#65306;</small>
-							<span>
-								{{ $task->examinees == 0 ? __('words.tasks.no_examinees') : sprintf('%03.1f', $task->solved / $task->examinees * 100) . '%' }}
-							</span>
-						</b>
-					</h2>
-				</div>
-			</div>
-		</div>
-		@component('components.alert')
+		@component('components.tasks_show', ['task' => $task])
 		@endcomponent
-		<h2><b>@lang('words.tasks.statement')</b></h2>
-		<p>{!! nl2br(e($task->statement)) !!}</p>
-		<h2><b>@lang('words.tasks.constraints')</b></h2>
-		<p>{!! nl2br(e($task->constraints)) !!}</p>
-		<h2><b>@lang('words.tasks.input')</b></h2>
-		<p>{{ $task->input }}</p>
-		<pre><code>{{ $task->input_code }}</code></pre>
-		<h2><b>@lang('words.tasks.output')</b></h2>
-		<p>{{ $task->output }}</p>
-		@isset($task->output_code)
-			<pre><code>{{ $task->output_code }}</code></pre>
-		@endisset
 		<hr>
 		@foreach ($task->samples as $key => $sample)
 			<h3><b>@lang('words.tasks.sample_input')&thinsp;{{ $key + 1 }}</b></h3>
